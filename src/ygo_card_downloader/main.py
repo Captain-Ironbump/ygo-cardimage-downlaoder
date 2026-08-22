71b21412ea237de0367944f016502886dae04f86
Lukas Linde <linde.lukas.99@gmail.com>
2026-08-22 22:47:14 +0200

    chore: transformed to pyproject structure

--- src/ygo_card_downloader/main.py
+++ src/ygo_card_downloader/main.py
@@ -6,17 +6,19 @@
 import argparse
 from ygo_card_downloader.validator import validate_input, validate_output
 
-if __name__ == "__main__":
+
+def main():
     parser = argparse.ArgumentParser(
         description="YGOPro Card images Download Script from the official YGOPro API"
     )
     parser.add_argument("ids", help="CSV or JSON file containing the IDs")
     parser.add_argument("destination", help="Destination Folder")
     parser.add_argument(
-        '-e', '--api-endpoint',
-        default='cards',
-        choices=['cards', 'cards_small', 'cards_cropped'],
-        help='API endpoint to use (default: cards)'
+        "-e",
+        "--api-endpoint",
+        default="cards",
+        choices=["cards", "cards_small", "cards_cropped"],
+        help="API endpoint to use (default: cards)",
     )
     args = parser.parse_args()
 
@@ -24,3 +26,7 @@ if __name__ == "__main__":
     output_folder = validate_output(args.destination)
 
     print(input_file, output_folder, args.api_endpoint)
+
+
+if __name__ == "__main__":
+    main()
